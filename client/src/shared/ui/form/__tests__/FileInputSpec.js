3cec4adfcf57d74f4f76b2398aa510a56337424c
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-03-09 17:54:30 +0100

    tests

--- client/src/shared/ui/form/__tests__/FileInputSpec.js
+++ client/src/shared/ui/form/__tests__/FileInputSpec.js
@@ -10,31 +10,130 @@
 
 import React from 'react';
 
-import { shallow } from 'enzyme';
+import {
+  mount,
+  shallow
+} from 'enzyme';
 
 import { FileInput } from '..';
 
+/* global sinon */
 
 describe('<FileInput>', function() {
 
   it('should render', function() {
     createFileInput();
   });
+
+
+  it('should add files', function() {
+
+    // given
+    const setFieldValue = sinon.spy();
+    const wrapper = createFileInput({
+      field: {
+        name: 'name',
+        value: []
+      },
+      form: {
+        setFieldValue
+      }
+    }, mount);
+    const input = wrapper.find('input');
+
+    // when
+    const newFiles = [ createFile() ];
+    setFiles(input, newFiles);
+    input.simulate('change');
+
+    // then
+    expect(setFieldValue).to.have.been.calledOnce;
+    expect(setFieldValue.args).to.deep.equal([
+      [ 'name', newFiles ]
+    ]);
+  });
+
+
+  it('should add only new files', function() {
+
+    // given
+    const initialValue = [ createFile() ];
+    const setFieldValue = sinon.spy();
+    const wrapper = createFileInput({
+      field: {
+        name: 'name',
+        value: initialValue
+      },
+      form: {
+        setFieldValue
+      }
+    }, mount);
+    const input = wrapper.find('input');
+
+    // when
+    setFiles(input, initialValue);
+    input.simulate('change');
+
+    // then
+    expect(setFieldValue).to.have.been.calledOnce;
+    expect(setFieldValue.args).to.deep.equal([
+      [ 'name', initialValue ]
+    ]);
+  });
+
+
+  it('should remove files', function() {
+
+    // given
+    const initialValue = [ createFile() ];
+    const setFieldValue = sinon.spy();
+    const wrapper = createFileInput({
+      field: {
+        name: 'name',
+        value: initialValue
+      },
+      form: {
+        setFieldValue
+      }
+    }, mount);
+
+    // when
+    wrapper.find('.remove').simulate('click');
+
+    // then
+    expect(setFieldValue).to.have.been.calledOnce;
+    expect(setFieldValue.args).to.deep.equal([
+      [ 'name', [] ]
+    ]);
+  });
 });
+
 
 
 // helpers ///////////////////
 
-function createFileInput(options = {}) {
+function createFileInput(options = {}, render = shallow) {
   const {
-    field,
-    form,
+    field = { value: [] },
+    form = {},
     ...props
   } = options;
 
-  return shallow(<FileInput
+  return render(<FileInput
     { ...props }
-    field={ field || {} }
-    form={ form || {} }
+    field={ field }
+    form={ form }
   />);
+}
+
+function createFile(options = {}) {
+  return {
+    name: 'file',
+    path: '/fake/path',
+    ...options
+  };
+}
+
+function setFiles(input, files) {
+  sinon.stub(input.getDOMNode(), 'files').value(files);
 }
